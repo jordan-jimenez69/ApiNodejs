8c1edc2ee0d64b295cfab8fd73ded35a82a0d2d6
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2024-04-11 10:55:34 +0200

    Aeration du code

--- TP-Final/src/Data/villes.js
+++
@@ -1,6 +0,0 @@
-/*export let villes = [
-    { id: 1, ville: "Tokyo", first: "Tokyo", second: "Tokyo", third: "Tokyo", fourth: "Tokyo", fifth: "Tokyo"},
-    { id: 2, ville: "Kyoto", first: "Tokyo", second: "Tokyo", third: "Tokyo", fourth: "Tokyo", fifth: "Tokyo"},
-    { id: 3, ville: "Osaka", first: "Tokyo", second: "Tokyo", third: "Tokyo", fourth: "Tokyo", fifth: "Tokyo"},
-    { id: 4, ville: "Nara", first: "Tokyo", second: "Tokyo", third: "Tokyo", fourth: "Tokyo", fifth: "Tokyo"},
-];*/
