2a0315b8c24a37c91791aed00a8b3db40314f23a
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2024-04-11 10:25:06 +0200

    Put, DELTE and getactiviteby ID fonctionelle

--- TP-Final/src/controller/activites.js
+++ TP-Final/src/controller/activites.js
@@ -1,5 +1,4 @@
 import Activite from "../models/activite.js";
-import Ville from "../models/ville.js";
 
 export const getActiviteByVille = async (req, res) => {
     try {
@@ -9,6 +8,22 @@ export const getActiviteByVille = async (req, res) => {
         const activites = await Activite.find({ ville: villeId });
 
         res.json(activites);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const getActiviteById = async (req, res) => {
+    try {
+        const activiteId = req.params.activiteId;
+
+        const activite = await Activite.findById(activiteId);
+        
+        if (!activite) {
+            return res.status(404).json({ message: "Activité non trouvée" });
+        }
+        
+        res.json(activite);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -34,3 +49,39 @@ export const addActivityToVille = async (req, res) => {
     }
 };
 
+export const updateActiviteById = async (req, res) => {
+    try {
+        const activiteId = req.params.activiteId;
+        const { name, adress, description } = req.body; 
+        
+        const activiteModifiee = await Activite.findByIdAndUpdate(activiteId, {
+            name,
+            adress,
+            description
+        }, { new: true }); 
+        
+        if (!activiteModifiee) {
+            return res.status(404).json({ message: "Activité non trouvée" });
+        }
+        
+        res.json(activiteModifiee);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const deleteActiviteById = async (req, res) => {
+    try {
+        const activiteId = req.params.activiteId;
+
+        const activiteSupprimee = await Activite.findByIdAndDelete(activiteId);
+        
+        if (!activiteSupprimee) {
+            return res.status(404).json({ message: "Activité non trouvée" });
+        }
+        
+        res.json({ message: "Activité supprimée avec succès" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
--- TP-Final/src/routes/activites.js
+++ TP-Final/src/routes/activites.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getActiviteByVille, addActivityToVille } from '../controller/activites.js';
+import { getActiviteByVille, addActivityToVille, updateActiviteById, deleteActiviteById, getActiviteById } from '../controller/activites.js';
 
 const router = express.Router();
 
@@ -7,6 +7,13 @@ const router = express.Router();
 router.get("/villes/:villeId", getActiviteByVille);
 
 router.post("/villes/:villeId", addActivityToVille);
+
+router.put("/villes/:villeId/:activiteId", updateActiviteById);
+
+router.delete("/villes/:villeId/:activiteId", deleteActiviteById);
+
+router.get("/villes/:villeId/:activiteId", getActiviteById);
+
 
 
 export default router;
